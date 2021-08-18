044dc5392ec91ae332fe16ed8a7bd7f77daa0085
Alankar Shukla <alankarshukla9@gmail.com>
2021-08-18 16:39:43 +0530

    Update schema_generator.py

--- schema_generator.py
+++ schema_generator.py
@@ -1,6 +1,3 @@
-
-
-
 import sqlite3
 from collections import OrderedDict
 
@@ -17,15 +14,24 @@ cur = db.cursor()
 result = cur.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
 table_names = list(zip(*result))[0]
 
-def column_json(table_names):
+def column_json(table_names, col_type):
   columns = [[-1,"*"],]
-  for i, table_name in enumerate(table_names):
-      result = cur.execute("PRAGMA table_info('%s')" % table_name).fetchall()
-      column_names = list(zip(*result))[1]
-      for j in column_names:
-        columns.append([i , j])
-  return columns
-
+  if col_type == 'org':
+    for i, table_name in enumerate(table_names):
+        result = cur.execute("PRAGMA table_info('%s')" % table_name).fetchall()
+        column_names = list(zip(*result))[1]
+        for j in column_names:
+          # print(j.title(), j.lower())
+          columns.append([i , j.title()])
+          print(columns)
+    return columns
+  if col_type == 'normal':
+    for i, table_name in enumerate(table_names):
+        result = cur.execute("PRAGMA table_info('%s')" % table_name).fetchall()
+        column_names = list(zip(*result))[1]
+        for j in column_names:
+          columns.append([i , j.lower()])
+    return columns
 def columns_datatype(table_names):
    col_data_type = []
    for table_name in table_names:
@@ -51,9 +57,9 @@ for i in sc_keys:
   elif i == 'table_names_original':
     db_schema["table_names_original"] =  [i.title() for i in list(table_names)]
   elif i == 'column_names':
-    db_schema["column_names"] = column_json(table_names)
+    db_schema["column_names"] = column_json(table_names,  'normal')
   elif i == 'column_names_original':
-    db_schema["column_names_original"] = column_json(table_names)
+    db_schema["column_names_original"] = column_json(table_names, 'org')
   elif i == 'column_types':
     db_schema["column_types"] = columns_datatype(table_names)
   elif i == 'db_id':
@@ -72,7 +78,9 @@ ordered = OrderedDict()
 for k in sc_keys:
     ordered[k] = db_schema[k]
 
-#Dumping as json
+# Dumping as json file
+
 import json
-print(json.dumps(db_schema))
+with open("demo.json", "w") as outfile:
+    json.dump(db_schema, outfile, indent=4)
 
